cfd1f0a55fd8e24e088043f5393fa21d3b2a243c
Seb1995 <99344424+scordesign@users.noreply.github.com>
2022-12-29 10:15:34 -0500

    inicial

---
+++ database/migrations/2022_12_29_134855_productos.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Productos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('productos', function (Blueprint $table) {
+
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+            $table->Integer('bodega_id')->unsigned();
+            $table->foreign('bodega_id')->references('id')->on('bodegas')->onDelete('cascade');
+            $table->Integer('grupo_id')->unsigned();
+            $table->foreign('grupo_id')->references('id')->on('grupos')->onDelete('cascade');
+            $table->timestamps();
+            $table->string('ubicacion');
+            $table->Integer('conteo1');
+            $table->Integer('conteo2');
+            $table->Integer('deferencia12');
+            $table->Integer('conteo3');
+        
+            
+
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
