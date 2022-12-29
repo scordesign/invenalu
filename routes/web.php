4f7d10d325543409d309cb77ba18442ada7adf21
Seb1995 <99344424+scordesign@users.noreply.github.com>
2022-12-29 11:54:06 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Auth::routes();
 Route::resource('bodegas', App\Http\Controllers\BodegaController::class);
 Route::resource('grupos', App\Http\Controllers\GrupoController::class);
 Route::resource('codigos', App\Http\Controllers\CodigoController::class);
-Route::resource('inventario', App\Http\Controllers\ProductoController::class);
+Route::resource('productos', App\Http\Controllers\ProductoController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
