1f734896015b74043e2b11b16aa28a25bbbbb01d
Seb1995 <99344424+scordesign@users.noreply.github.com>
2022-12-29 14:04:10 -0500

    Update index.blade.php

--- resources/views/ubicacione/index.blade.php
+++ resources/views/ubicacione/index.blade.php
@@ -18,7 +18,7 @@
 
                              <div class="float-right">
                                 <a href="{{ route('ubicaciones.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __(Crear nueva') }}
+                                  {{ __('Crear nueva') }}
                                 </a>
                               </div>
                         </div>
